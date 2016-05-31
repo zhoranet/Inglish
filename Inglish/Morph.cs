ab7f51c4a052379c2a2afc55a1e5680fa2e3d9a7
zhoranet <job@zhora.net>
2016-05-30 22:55:29 -0700

    code cleanup

--- Inglish/Morph.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Inglish
-{
-    public class Morph
-    {
-        public MorphType MorphType { get; set; }
-        public int Index { get; set; }
-        public string Value { get; set; }
-    }
-}
